a01873b2cb270912476529fba8d14ed150cca9c6
ipr9n <57600027+ipr9n@users.noreply.github.com>
2019-11-15 20:15:18 +0300

    Начальная версия.

---
+++ Tetris/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Tetris
+{
+    public partial class Form1 : Form
+    {
+        private TetrisGame tetrisGame = new TetrisGame();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 300;
+            pictureBox1.Location = new Point(1,1);
+            pictureBox1.Size = new Size(500,1000);
+            this.Size = pictureBox1.Size;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            tetrisGame.MoveBlock(e.KeyCode);
+
+            if (!timer1.Enabled)
+                timer1.Start();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            tetrisGame.Draw(e.Graphics);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tetrisGame.Update();
+            pictureBox1.Refresh();
+        }
+    }
+}
---
+++ Tetris/TetrisGame.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    class TetrisGame
+    {
+        private int squareSize = 20;
+        private List<Point> zanyato = new List<Point>();
+        private int x = 5, y = 1;
+        private Point boxLocation;
+
+        public void MoveBlock(Keys direction)
+        {
+            switch (direction)
+            {
+                case Keys.Left:
+                    x -= 1;
+                    break;
+                case Keys.Right:
+                    x += 1;
+                    break;
+
+            }
+        }
+
+        public void Update()
+        {
+            y += 1;
+            if (zanyato.Contains(boxLocation) || y>19)
+            {
+                zanyato.Add(new Point(boxLocation.X, boxLocation.Y));
+                x = 5;
+                y = 1;
+            }
+            boxLocation = new Point(x, y);
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            for (int x = 0; x <= 10; x++)
+                graphics.DrawLine(Pens.Black, x * squareSize, 0, x * squareSize, 400);
+
+            for (int y = 0; y <= 20; y++)
+                graphics.DrawLine(Pens.Black, 0, y * squareSize, 200, y * squareSize);
+
+            for (int z = 0; z < zanyato.Count; z++)
+                graphics.FillRectangle(Brushes.Blue, zanyato[z].X * squareSize, zanyato[z].Y * squareSize, squareSize, squareSize);
+
+            graphics.FillRectangle(Brushes.Blue, boxLocation.X * squareSize, boxLocation.Y * squareSize, squareSize, squareSize);
+        }
+    }
+}
